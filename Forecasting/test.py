690200c1dd5c525666f1ea6ba66ce52bcad2f9ff
Marcos Heredia <marcosherediapimienta@gmail.com>
2024-05-25 20:50:21 +0200

    new

--- Forecasting/test.py
+++ Forecasting/test.py
@@ -1,6 +1,10 @@
 from data_loading.data import LoadingData
 from Models.arima.arima import ARIMA
-from ts_tools.tools import tools
+from Forecasting.ts_tools.tools.tools import tools
+from ts_tools.Dickey_fuller.DF import adf_test
+from ts_tools.Dickey_fuller.DF import difference_series
+from ts_tools.Dickey_fuller.DF import log_difference_series
+
 
 # Load the data
 loader = LoadingData(tickers=['AAPL'])
@@ -11,6 +15,24 @@ ts_tools = tools()
 ts = ts_tools.ts_prepartion(ts, 'Date', 'Adj Close')
 ts_tools.plot(ts)
 
+ts_diff = difference_series(ts, 'Adj Close')
 
+# Realizamos el test de Dickey-Fuller en la serie diferenciada
+adf_result = adf_test(ts_diff)
+
+# Si la serie diferenciada no es estacionaria, aplicamos la diferenciación logarítmica
+if adf_result[1] > 0.05:
+    ts_log_diff = log_difference_series(ts, 'Adj Close')
+    adf_log_result = adf_test(ts_log_diff)
+    ts_diff = ts_log_diff
+
+# Graficar la serie diferenciada (o log-diferenciada)
+plt.figure(figsize=(10, 6))
+plt.plot(ts_diff, label='Differenced Series')
+plt.title('Differenced Time Series')
+plt.xlabel('Date')
+plt.ylabel('Differenced Value')
+plt.legend()
+plt.show()
 
 
---
+++ Forecasting/ts_tools/Dickey_fuller/DF.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+import matplotlib.pyplot as plt
+
+# Diferenciación de la serie temporal
+def difference_series(ts, column):
+    ts_diff = ts[column].diff().dropna()
+    return ts_diff
+
+# Aplicar test de Dickey-Fuller
+def adf_test(series):
+    result = adfuller(series)
+    print('ADF Statistic:', result[0])
+    print('p-value:', result[1])
+    for key, value in result[4].items():
+        print(f'Critical Value ({key}): {value}')
+    return result
+
+# Diferenciación logarítmica
+def log_difference_series(ts, column):
+    ts_log_diff = np.log(ts[column]).diff().dropna()
+    return ts_log_diff
