d101eddcfac51aafeff6535b884851f16a610511
Marcos Heredia <marcosherediapimienta@gmail.com>
2024-05-25 13:25:12 +0200

    changes

--- Forecasting/test.py
+++ Forecasting/test.py
@@ -1,8 +1,13 @@
 from data_loading.data import LoadingData
 from Models.arima.arima import ARIMA
+from ts_tools.tools import tools
 
 # Load the data
 loader = LoadingData(tickers=['AAPL'])
 ts = loader.get_data()
 info = loader.get_info_ticker()
+
+ts_tools = tools()
+ts = ts_tools.ts_prepartion(ts, 'Date', 'Adj Close')
+ts_tools.plot(ts)
 
